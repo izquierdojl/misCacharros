bcfdd7de26805a3a3bcc4cde1131cde194c2d57c
izquierdojl <izquierdo.jl@gmail.com>
2024-02-18 11:24:23 +0100

    Clase aplicación para guardar objetos de uso general

--- app/src/main/java/com/jlib/miscacharros/Aplicacion.java
+++ app/src/main/java/com/jlib/miscacharros/Aplicacion.java
@@ -1,9 +1,10 @@
 package com.jlib.miscacharros;
 
 import android.app.Application;
-
 import com.jlib.miscacharros.datos.RepositorioTipos;
 import com.jlib.miscacharros.datos.TiposLista;
+
+// esta clase inicializa otras para ser utilizada durante la vida de la aplicación
 
 public class Aplicacion extends Application {
 
